a01476150245356f78ed51ada3d0608ea3655794
Karl <k.dougla01@pudseygrangefield.co.uk>
2019-12-17 20:57:33 +0000

    v0.2
    Added html templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -9,24 +9,22 @@ def query_example():
 	framework = request.args['framework'] #if key doesn't exist, returns a 400, bad request error
 	website = request.args.get('website')
 
-	return '''<h1>The language value is: {}</h1>
-			  <h1>The framework value is: {}</h1>
-			  <h1>The website value is: {}'''.format(language, framework, website)
+	return render_template('query.html', language=language, \
+										framework=framework, \
+										website=website)	
+
 
 @app.route('/form-example', methods=['GET', 'POST']) #allow both GET and POST requests
 def form_example():
 	if request.method == 'POST':  #this block is only entered when the form is submitted
 		language = request.form.get('language')
-		framework = request.form['framework']
+		framework = request.form.get('framework')
 
 		return '''<h1>The language value is: {}</h1>
 				  <h1>The framework value is: {}</h1>'''.format(language, framework)
 
-	return '''<form method="POST">
-				  Language: <input type="text" name="language"><br>
-				  Framework: <input type="text" name="framework"><br>
-				  <input type="submit" value="Submit"><br>
-			  </form>'''
+	return render_template('form.html')	
+
 
 @app.route('/json-example', methods=['POST']) #GET requests will be blocked
 def json_example():
@@ -42,13 +40,11 @@ def json_example():
 	example = req_data['examples'][0] #an index is needed because of the array
 	boolean_test = req_data['boolean_test']
 
-	return '''
-		   The language value is: {}
-		   The framework value is: {}
-		   The Python version is: {}
-		   The item at index 0 in the example list is: {}
-		   The boolean value is: {}'''.format(language, framework, python_version, example, boolean_test)
-
+	return render_template('json.html', language=language, \
+										framework=framework, \
+										python_version=python_version, \
+										example=example, \
+										boolean_test=boolean_test)
 
 if __name__ == '__main__':
 	app.run(debug=True, port=5000, host='127.0.0.1')
